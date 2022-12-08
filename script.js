aa16f94f7a51d9092150594fea8ffcdb0290ee12
GEORGE-ARTUR MUSTATA <mustata.da.georgeartur21@stud.rau.ro>
2022-12-09 00:33:31 +0200

    update9122022

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-let selectedFile= document.getElementById('uploadFile').files[0];
+function display_random_image() 
+{
+     var theImages = [{
+        src: "images/Screenshot_20221123-011715_Instagram.jpg",
+        width: "1080",
+        height: "2408"
+        
+        
+    }, {
+        src: "images/Screenshot_20221123-011818_Instagram.jpg",
+        
+    }, {
+        src: "images/Screenshot_20221123-011900_Instagram.jpg",
+        
+    }];
+    
+    var preBuffer = [];
+    for (var i = 0, j = theImages.length; i < j; i++) {
+        preBuffer[i] = new Image();
+        preBuffer[i].src = theImages[i].src;
+        preBuffer[i].width = theImages[i].width;
+        preBuffer[i].height = theImages[i].height;
+    }
+   
+// create random image number
+  function getRandomInt(min,max) 
+    {
+      //  return Math.floor(Math.random() * (max - min + 1)) + min;
+    
+imn = Math.floor(Math.random() * (max - min + 1)) + min;
+    return preBuffer[imn];
+    }  
+
+// 0 is first image,   preBuffer.length - 1) is  last image
+  
+var newImage = getRandomInt(0, preBuffer.length - 1);
+ 
+// remove the previous images
+var images = document.getElementsByTagName('img');
+var l = images.length;
+for (var p = 0; p < l; p++) {
+    images[0].parentNode.removeChild(images[0]);
+}
+// display the image  
+document.body.appendChild(newImage);
+}
